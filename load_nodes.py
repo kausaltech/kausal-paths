b23da8dab9f6bb8ba6ce79b6358a0b448ac2209c
Juha Yrjölä <juha.yrjola@iki.fi>
2021-06-08 18:39:44 +0300

    Implement a CLI to load_nodes.py

--- load_nodes.py
+++ load_nodes.py
@@ -1,11 +1,26 @@
+import argparse
 import sys
 from nodes.instance import InstanceLoader
+
+
+if True:
+    # Print traceback for warnings
+    import traceback
+    import warnings
+
+    def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+        log = sys.stderr
+        traceback.print_stack()
+        log.write(warnings.formatwarning(message, category, filename, lineno, line))
+
+    warnings.showwarning = warn_with_traceback
+
 
 loader = InstanceLoader('configs/tampere.yaml')
 loader.print_graph()
 context = loader.context
 
-page = list(loader.pages.values())[0]
+page = list(loader.instance.pages.values())[0]
 
 
 def print_metric(metric):
@@ -27,13 +42,24 @@ def print_metric(metric):
         print(val)
 
 
-if len(sys.argv) > 1:
-    node_id = sys.argv[1]
-    node = context.get_node(node_id)
-    print(node.get_output())
-    exit()
+parser = argparse.ArgumentParser(description='Execute the computational graph')
+parser.add_argument('--param', action='append', type=str, help='set a parameter')
+parser.add_argument('--list-params', action='store_true', help='list parameters')
+parser.add_argument('--node', type=str, nargs='+', help='Compute node')
+args = parser.parse_args()
 
-if True:
+if args.list_params:
+    context.print_params()
+
+for param_arg in (args.param or []):
+    param_id, val = param_arg.split('=')
+    context.set_param_value(param_id, val)
+
+for node_id in (args.node or []):
+    node = context.get_node(node_id)
+    node.print_output()
+
+if False:
     loader.context.dataset_repo.pull_datasets()
     loader.context.print_params()
     loader.context.generate_baseline_values()
