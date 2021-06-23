7e68a309a8ef0d866e2e3d59869f1b4bc0715f6c
Bernhard Bliem <bernhard@bliem.xyz>
2021-06-23 12:36:45 +0300

    Add tests

--- paths/tests/test_schema.py
+++ paths/tests/test_schema.py
@@ -1,8 +1,47 @@
 import pytest
 
-from nodes.tests.factories import ActionNodeFactory, ScenarioFactory
+from params.tests.factories import NumberParameterFactory
 
 pytestmark = pytest.mark.django_db
+
+
+def test_parameter_interface(graphql_client_query_data, context, action_node):
+    param = NumberParameterFactory()
+    action_node.register_param(param)
+    data = graphql_client_query_data(
+        '''
+        query($param: ID!) {
+          parameter(id: $param) {
+            __typename
+            id
+            label
+            description
+            nodeRelativeId
+            node {
+              __typename
+            }
+            isCustomized
+            isCustomizable
+          }
+        }
+        ''',
+        variables={'param': param.id}
+    )
+    expected = {
+        'parameter': {
+            '__typename': 'NumberParameterType',
+            'id': param.id,
+            'label': str(param.label),
+            'description': str(param.description),
+            'nodeRelativeId': param.node_relative_id,
+            'node': {
+                '__typename': 'NodeType'
+            },
+            'isCustomized': param.is_customized,
+            'isCustomizable': param.is_customizable,
+        }
+    }
+    assert data == expected
 
 
 def test_action_enabled(graphql_client_query_data, context, action_node):
