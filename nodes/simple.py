a539d5c0bd7e564ae121a5ce67870fa50e9cadb8
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2021-07-15 11:30:47 +0300

    New HIA code; not sure how these relate to the branch hia.

--- nodes/simple.py
+++ nodes/simple.py
@@ -2,6 +2,9 @@ from re import T
 from params.param import BoolParameter, NumberParameter
 from typing import List
 import pandas as pd
+import pint
+from .context import unit_registry
+import numpy as np
 
 from common.i18n import gettext_lazy as _
 from .constants import FORECAST_COLUMN, VALUE_COLUMN
@@ -230,3 +233,56 @@ class FixedMultiplierNode(SimpleNode):
             df = self.replace_output_using_input_dataset(df)
 
         return df
+
+class ExposureNode(AdditiveNode):
+    quantity = 'exposure'
+    """Simple addition of exposure"""
+    pass
+
+class PopulationAttributableFractionNode(AdditiveNode):
+    """Calculate population attributable fraction (PAF) from relative risk (RR)
+    and fraction of population exposed.
+    """
+
+    def compute(self):
+
+        if len(self.input_nodes) != 2:
+            raise NodeError(self, "Must receive exactly two inputs in this order: relative risk and fraction exposed")
+
+        n1, n2 = self.input_nodes
+        output_unit = n1.unit * n2.unit
+        if not self.is_compatible_unit(output_unit, self.unit):
+            raise NodeError(self, "Inputs must in a unit compatible with '%s'" % self.unit)
+
+        df1 = n1.get_output()
+        df2 = n2.get_output()
+        df = df1.copy()
+
+        if self.debug:
+            print('%s: Multiply input from node 1 (%s):' % (self.id, n1.id))
+            self.print_pint_df(df1)
+            print('%s: Multiply input from node 2 (%s):' % (self.id, n2.id))
+            self.print_pint_df(df2)
+        
+        r = df2[VALUE_COLUMN] * (df1[VALUE_COLUMN] - 1 * unit_registry('meter')) # FIXIT should be unitless, not meters
+        df[VALUE_COLUMN] = np.where(r>0, r/(r + 1 * unit_registry('meter ** 2')),r)
+
+        df[FORECAST_COLUMN] = df1[FORECAST_COLUMN] | df2[FORECAST_COLUMN]
+
+#        df[VALUE_COLUMN] = df[VALUE_COLUMN].pint.to(self.unit)
+
+        fill_gaps = self.get_param_value('fill_gaps_using_input_dataset', local=True, required=False)
+        if fill_gaps:
+            df = self.fill_gaps_using_input_dataset(df)
+        replace_output = self.get_param_value('replace_output_using_input_dataset', local=True, required=False)
+        if replace_output:
+            df = self.replace_output_using_input_dataset(df)
+        if self.debug:
+            print('%s: Output:' % self.id)
+            self.print_pint_df(df)
+
+        df[FORECAST_COLUMN] = df[FORECAST_COLUMN].astype(bool)
+
+        return df
+
+
