8cdb8151faac986b29a9e5a6e149062a38ae19ab
Juha Yrjölä <juha.yrjola@iki.fi>
2021-10-27 19:26:14 +0300

    Fix nodes/__init__.py

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,6 +1,8 @@
 from .context import Context
-from .node import Node
+from .node import Node, NodeError
 from .datasets import Dataset, DVCDataset, FixedDataset
 
 
-__all__ = ['Dataset', 'Context', 'Node', 'DVCDataset', 'FixedDataset']
+__all__ = [
+    'Dataset', 'Context', 'Node', 'DVCDataset', 'FixedDataset', 'NodeError',
+]
