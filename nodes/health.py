b180f955de57daf60920a39fe0c76c2e183914a6
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2021-11-05 20:56:48 +0200

    OvaOps changed to OvariableFrame. Still bugs in heaalth.py

--- nodes/health.py
+++ nodes/health.py
@@ -18,7 +18,7 @@ from .node import Context, Node
 from .exceptions import NodeError
 
 from .simple import AdditiveNode, FixedMultiplierNode, SimpleNode
-from .ovariable import Ovariable, Ovariable2
+from .ovariable import Ovariable, OvariableFrame
 
 # ############################33
 # Health-related constants and classes
@@ -40,27 +40,32 @@ class Exposure(Ovariable):
     def compute(self):
         consumption = self.prepare_ovariable('ingestion')
         concentration = self.prepare_ovariable('mass_concentration')
+        er_function = self.prepare_ovariable('exposure-response',
+            query = 'observation=="param1"', drop = ['observation', 'Response'])
 
         exposure = concentration * consumption
+
+        er_function = er_function * 0
+        er_function.print_pint_df()
+        exposure = exposure + er_function
 
         return self.clean_computing(exposure)
 
     # scale_exposure() scales the exposure by logarithmic function or body weight.
     # The information about how to scale comes from exposure-response function.
     # Thus, er-function and body weight must be provided.
-    # All ovariables must have contents calculated.
     # FIXME Is this a sensible thing to do, as then the units and dimensions may differ within node?
 
     def scale_exposure(self, erf, bw):
-        if 'er_function' in self.content.index.names:
-            return self
+        exposure = OvariableFrame(self.get_output())
+        if 'er_function' in exposure.index.names:
+            return exposure
 
-        out = copy.copy(erf)
-        out.content = copy.copy(out.content)
-        out.content[VALUE_COLUMN] = out.content[VALUE_COLUMN].pint.m
+        out = erf.copy()
+        out[VALUE_COLUMN] = out[VALUE_COLUMN].pint.m
         out = out * 0
 
-        out = out + self
+        out = out + exposure
 
         out = out.merge(bw).reset_index()
 
@@ -78,10 +83,9 @@ class Exposure(Ovariable):
         keep = set(out.columns) - {0, VALUE_x, VALUE_y, FORECAST_x, FORECAST_y}
         out = out[list(keep)].set_index(list(keep - {VALUE_COLUMN, FORECAST_COLUMN}))
 
-        self.content_orig = self.content
-        self.content = out
+        # self_orig = self
 
-        return self
+        return out.clean()
 
 
 class FixedMultiplierHealthImpactNode(FixedMultiplierNode):  # Needed for pre-ovariable nodes
@@ -121,8 +125,8 @@ class ExposureResponseFunction(Ovariable):
         df = pd.DataFrame([
             ['None', 'cardiovascular disease', 'RR', 'param1', False, 200],
             ['None', 'cardiovascular disease', 'RR', 'param2', False, 0],
-            ['None', 'cerebrovascular disease', 'Relative Hill', 'param1', False, 2],
-            ['None', 'cerebrovascular disease', 'Relative Hill', 'param2', False, 0.2],
+            # ['None', 'cerebrovascular disease', 'Relative Hill', 'param1', False, 2],
+            # ['None', 'cerebrovascular disease', 'Relative Hill', 'param2', False, 0.2],
             # ['None', 'dioxin', 'Step', 'param1', False, 20],
             # ['None', 'dioxin', 'Step', 'param2', False, 0],
             ['None', 'cancer', 'UR', 'param1', False, 2000],
@@ -153,43 +157,44 @@ class RelativeRisk(Ovariable):
         bw = self.prepare_ovariable('body_weight')
 
         exposure = self.prepare_ovariable('ingestion')
-        exposure = exposure.scale_exposure(param1, bw)
+        # exposure = self.scale_exposure(exposure, param1, bw)  # FIXME Think a new solution to scaling
 
         df = pd.DataFrame()
-        relative_functions = set(exposure.content.reset_index().er_function)
-        relative_functions = sorted(relative_functions & {'RR', 'Relative Hill'})
+#        relative_functions = set(self.get_output().reset_index().er_function)   # Contains also unused functions
+        relative_functions = {'RR'}  # sorted(relative_functions & {'RR', 'Relative Hill'})
 
         for func in relative_functions:
 
             if func == 'RR':
-                beta = param1 ** -1  # FIXME This is stupid parameterization
-                beta.content = beta.content.query("er_function == 'RR'")
+                beta = param1.query("er_function == 'RR'")
+                beta = 1 / beta  # FIXME This is stupid parameterization
 
                 threshold = param2
 
                 dose2 = exposure - threshold
 
                 #  Smallest allowed value is 0 FIXME: Not if scaling: Log10
-                dose2.content = np.clip(dose2.content, 0, None)
+                dose2 = np.clip(dose2, 0, None)
                 out1 = beta * dose2
-                out1.content[VALUE_COLUMN] = out1.content[VALUE_COLUMN].pint.m_as('')
+                out1[VALUE_COLUMN] = out1[VALUE_COLUMN].pint.m_as('')
 
-                out1 = out1.exp()
-                df = df.append(out1.content.reset_index())
+                out1 = OvariableFrame(out1).exp()
+                df = df.append(out1.reset_index())
 
             elif func == 'Relative Hill':
                 Imax = param1
-                Imax.content = Imax.content.query("er_function == 'Relative Hill'")
-                Imax.content[VALUE_COLUMN] = Imax.content[VALUE_COLUMN].pint.m
+                Imax = Imax.query("er_function == 'Relative Hill'")
+                Imax[VALUE_COLUMN] = Imax[VALUE_COLUMN].pint.m
 
                 ed50 = param2
 
                 out2 = (exposure * Imax) / (exposure + ed50) + 1
 
-                df = df.append(out2.content.reset_index())
+                df = df.append(out2.reset_index())
 
         keep = set(df.columns) - {'er_function', 'scaling'}
         df = df[list(keep)].set_index(list(keep - {VALUE_COLUMN, FORECAST_COLUMN}))
+        OvariableFrame(df).print_pint_df()
 
         return self.clean_computing(df)
 
@@ -214,8 +219,8 @@ class PopulationAttributableFraction(Ovariable):
         p_illness = self.prepare_ovariable('probability')
         bw = self.prepare_ovariable('body_weight')
 
-        exposure = exposure.scale_exposure(param1, bw)
-        er_function_list = list(sorted(set(exposure.content.reset_index()['er_function'])))
+        # exposure = self.scale_exposure(exposure, param1, bw)
+        er_function_list = list(sorted(set(exposure.reset_index()['er_function'])))
         if 'RR' in er_function_list:
             er_function_list = list(sorted(set(er_function_list) - {'Relative Hill'}))
             # You don't want to do twice
@@ -226,25 +231,25 @@ class PopulationAttributableFraction(Ovariable):
 
             if func == 'UR':
                 k = copy.copy(param1)
-                k.content = k.content.copy().query("er_function == 'UR'")
+                k = k.copy().query("er_function == 'UR'")
                 k = k ** -1
 
                 threshold = param2
 
                 dose2 = (exposure - threshold)
                 # FIXME clip removes the pint unit. Figure out something else.
-                # dose2.content = np.clip(dose2.content, 0, None)  # Smallest allowed value is 0
+                # dose2 = np.clip(dose2, 0, None)  # Smallest allowed value is 0
                 out1 = (k * dose2 * frexposed / incidence)
-                out = out.append(out1.content.reset_index())
+                out = out.append(out1.reset_index())
 
             elif func == 'Step':
                 upper = copy.copy(param1)
-                upper.content = upper.content.copy().query("er_function == 'Step'")
+                upper = upper.copy().query("er_function == 'Step'")
 
                 lower = param2
                 out2 = (exposure >= lower) * (exposure <= upper) * -1 + 1  # FIXME
                 out2 = out2 * frexposed / incidence
-                out = out.append(out2.content.reset_index())
+                out = out.append(out2.reset_index())
 
             elif func == 'RR' or func == 'Relative Hill':
                 r = frexposed * (rr - 1)
@@ -252,28 +257,28 @@ class PopulationAttributableFraction(Ovariable):
                 out3 = (r / (r + 1))  # AF=r/(r+1) if r >= 0; AF=r if r<0. Therefore, if the result
                 # is smaller than 0, we should use r instead. It can be converted from the result:
                 # r/(r+1)=a <=> r=a/(1-a)
-                out3.content[VALUE_COLUMN] = np.where(
-                    out3.content[VALUE_COLUMN] < 0,
-                    out3.content[VALUE_COLUMN] / (1 - out3.content[VALUE_COLUMN]),
-                    out3.content[VALUE_COLUMN])
+                out3[VALUE_COLUMN] = np.where(
+                    out3[VALUE_COLUMN] < 0,
+                    out3[VALUE_COLUMN] / (1 - out3[VALUE_COLUMN]),
+                    out3[VALUE_COLUMN])
 
-                out = out.append(out3.content.reset_index())
+                out = out.append(out3.reset_index())
 
             elif func == 'beta poisson approximation':
                 out4 = ((exposure / param2 + 1) ** (param1 * -1) * -1 + 1) * frexposed
                 out4 = (out4 / incidence * p_illness)
-                out = out.append(out4.content.reset_index())
+                out = out.append(out4.reset_index())
 
             elif func == 'exact beta poisson':
                 out5 = ((param1 / (param1 + param2) * exposure * -1).exp() * -1 + 1) * frexposed
                 out5 = out5 / incidence * p_illness
-                out = out.append(out5.content.reset_index())
+                out = out.append(out5.reset_index())
 
             elif func == 'exponential':
                 k = param1
                 out6 = ((k * exposure * -1).exp() * -1 + 1) * frexposed
                 out6 = out6 / incidence * p_illness
-                out = out.append(out6.content.reset_index())
+                out = out.append(out6.reset_index())
 
         keep = set(out.columns) - {'scaling', 'matrix', 'exposure', 'exposure_unit', 'er_function', 0}
         out = out[list(keep)].set_index(list(keep - {VALUE_COLUMN, FORECAST_COLUMN}))
@@ -292,7 +297,7 @@ class DiseaseBurden(Ovariable):
         case_burden = self.prepare_ovariable('case_burden')
 
         print('incidence', incidence.unit)
-        print(incidence.print_pint_df(incidence.content[0:2]))
+        print(incidence.print_pint_df(incidence[0:2]))
         out = population * incidence * case_burden
 
         return self.clean_computing(out)
