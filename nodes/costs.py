b49f92d9e04d49c422dff3df41b636593497471e
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2022-02-04 14:28:44 +0200

    Version with total investment and years for operation

--- nodes/costs.py
+++ nodes/costs.py
@@ -9,7 +9,7 @@ from .ovariable import Ovariable, OvariableFrame
 DISCOUNT_RATE = 0.03
 
 
-class CostNode(AdditiveNode):
+class CostNode(Ovariable):
     allowed_parameters = [
         NumberParameter(local_id='investment_lifetime'),
         NumberParameter(local_id='investment_cost'),
@@ -22,20 +22,32 @@ class CostNode(AdditiveNode):
     quantity = 'monetary_amount'
 
     def compute(self):
-        df = pd.DataFrame({
-            # YEAR_COLUMN
-            YEAR_COLUMN: [
-                2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026,
-                2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 3035
-            ],
-            FORECAST_COLUMN: [True] * 19,
-            VALUE_COLUMN: [unit_registry('0 EUR')] * 19
-        }).set_index(YEAR_COLUMN)
-        addition = self.get_parameter_value('investment_cost') * self.get_parameter('investment_cost').unit
+        costs = self.get_input('monetary_amount')
+        investment_cost = self.get_parameter_value('investment_cost') * self.get_parameter('investment_cost').unit
         investment_year = self.get_parameter_value('investment_year')
-        df.at[investment_year, VALUE_COLUMN] = addition + df.at[investment_year, VALUE_COLUMN]
-        self.print_pint_df(df)
-        print(self.unit)
-        df[VALUE_COLUMN] = self.ensure_output_unit(df[VALUE_COLUMN])
+        operation_cost = self.get_parameter_value('operation_cost') * self.get_parameter('operation_cost').unit
+        operation_year_start = self.get_parameter_value('operation_year_start')
+        operation_year_end = self.get_parameter_value('operation_year_end')
+        print(investment_year)
 
-        return(df)
+        discount = 1
+
+        for i in costs.reset_index()[YEAR_COLUMN]:
+            print(i)
+            change = unit_registry('0 kEUR')
+            if i == investment_year:
+                change = change + investment_cost
+            if i >= operation_year_start and i <= operation_year_end:
+                change = change + operation_cost
+            print(change)
+            if costs.at[i, FORECAST_COLUMN]:
+                discount = discount * (1 + DISCOUNT_RATE)
+            costs.at[i, VALUE_COLUMN] = (change + costs.at[i, VALUE_COLUMN]) / discount
+
+
+
+        self.print_pint_df(costs)
+        print(self.unit)
+        #  df[VALUE_COLUMN] = self.ensure_output_unit(df[VALUE_COLUMN])
+
+        return(costs)
