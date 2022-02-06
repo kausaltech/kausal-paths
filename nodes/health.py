0482fb1055a76ce8482efe65619f4a3fe0041a28
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2022-02-06 11:26:23 +0200

    Microbial ERFs added. NOT TESTED

--- nodes/health.py
+++ nodes/health.py
@@ -228,6 +228,23 @@ class PopulationAttributableFraction(Ovariable):
                 out = (tmp * Imax) / (exposure2 + ed50) + 1
                 out = postprocess_relative(rr=out, frexposed=frexposed)
 
+            elif erf_type == 5:  # beta poisson approximation
+                p1 = unit_registry('erf_param_ingestion').to('mg/kg/d', erf_context)
+                p2 = unit_registry('erf_param_scale').to('dimensionless', erf_context)
+                out = ((exposure / p2 + 1) ** (p1 * -1) * -1 + 1) * frexposed
+                out = (out / incidence * p_illness)
+
+            elif erf_type == 6:  # exact beta poisson # FIXME Logical error with units!
+                p1 = unit_registry('erf_param_ingestion').to('mg/kg/d', erf_context)
+                p2 = unit_registry('erf_param_ingestion2').to('mg/kg/d', erf_context)
+                out = ((p1 / (p1 + p2) * exposure * -1).exp() * -1 + 1) * frexposed
+                out = out / incidence * p_illness
+
+            elif erf_type == 7:  # exponential
+                k = unit_registry('erf_param_inv_ingestion').to('kg d/mg', erf_context)
+                out = ((k * exposure * -1).exp() * -1 + 1) * frexposed
+                out = out / incidence * p_illness
+
             else:
                 out = exposure2 / exposure
 
@@ -240,101 +257,6 @@ class PopulationAttributableFraction(Ovariable):
         print('PopulationAttributableFraction: ' + self.id)
         self.print_pint_df(output)
         return output
-
-    def compute_old(self):
-        param1 = 1  # self.get_input(
-        #    'exposure-response',
-        #    query="observation =='param1'", drop='observation')
-        param2 = 1  # self.get_input(
-        #    'exposure-response',
-        #    query="observation == 'param2'", drop='observation')
-        exposure = self.get_input('ingestion')
-        frexposed = self.get_input('fraction')
-        incidence = self.get_input('incidence')
-        p_illness = self.get_input('probability')
-
-        def postprocess_relative(rr, frexposed):
-            r = frexposed * (rr - 1)
-
-            out3 = (r / (r + 1))  # AF=r/(r+1) if r >= 0; AF=r if r<0. Therefore, if the result
-            # is smaller than 0, we should use r instead. It can be converted from the result:
-            # r/(r+1)=a <=> r=a/(1-a)
-            out3[VALUE_COLUMN] = np.where(
-                out3[VALUE_COLUMN] < 0,
-                out3[VALUE_COLUMN] / (1 - out3[VALUE_COLUMN]),
-                out3[VALUE_COLUMN])
-
-            return out3
-
-        er_function_list = exposure.do_inner_join(param2).reset_index()
-        er_function_list = list(sorted(set(er_function_list['er_function'])))
-
-        out = pd.DataFrame()
-
-        for func in er_function_list:
-
-            # FIXME Tähän scale_exposure
-
-            # FIXME Tähän yksikön tarkistus. ERF-dataan annetaan altistuksen yksikkö ilman log-muunnosta.
-
-            if func == 'UR':
-                k = OvariableFrame(param1.query("er_function == 'UR'"))
-                k = k ** -1
-                threshold = param2
-                dose2 = (exposure - threshold)
-                # FIXME clip removes the pint unit. Figure out something else.
-                # dose2 = np.clip(dose2, 0, None)  # Smallest allowed value is 0
-                out1 = k * dose2 * frexposed  # / incidence
-                out = out.append(out1.reset_index())
-
-            if func == 'Step':
-                upper = OvariableFrame(param1.query("er_function == 'Step"))
-                lower = param2
-                out2 = (exposure >= lower) * (exposure <= upper) * -1 + 1  # FIXME
-                out2 = out2 * frexposed / incidence
-                out = out.append(out2.reset_index())
-
-            if func == 'RR':
-                beta = OvariableFrame(param1.query("er_function == 'RR'"))
-                beta = beta ** -1  # FIXME This is stupid parameterization
-                threshold = param2
-                dose2 = exposure - threshold
-                #  Smallest allowed value is 0 FIXME: Not if scaling: Log10
-                #  dose2[VALUE_COLUMN] = np.clip(dose2[VALUE_COLUMN], 0, None)
-                out1 = beta * dose2
-                out1 = out1.exp()
-                out1 = postprocess_relative(rr=out1, frexposed=frexposed)
-                out = out.append(out1.reset_index())
-
-            if func == 'Relative Hill':
-                Imax = OvariableFrame(param1.query("er_function == 'Relative Hill'"))
-                Imax[VALUE_COLUMN] = Imax[VALUE_COLUMN].pint.m
-                ed50 = param2
-                out2 = (exposure * Imax) / (exposure + ed50) + 1
-                out = out.append(out2.reset_index())
-
-            if func == 'beta poisson approximation':
-                p1 = OvariableFrame(param1.query("er_function == 'beta poisson approximation"))
-                out4 = ((exposure / param2 + 1) ** (p1 * -1) * -1 + 1) * frexposed
-                out4 = (out4 / incidence * p_illness)
-                out = out.append(out4.reset_index())
-
-            if func == 'exact beta poisson':
-                p1 = OvariableFrame(param1.query("er_function == 'exact beta poisson'"))
-                out5 = ((p1 / (p1 + param2) * exposure * -1).exp() * -1 + 1) * frexposed
-                out5 = out5 / incidence * p_illness
-                out = out.append(out5.reset_index())
-
-            if func == 'exponential':
-                k = OvariableFrame(param1.query("er_function == 'exponential'"))
-                out6 = ((k * exposure * -1).exp() * -1 + 1) * frexposed
-                out6 = out6 / incidence * p_illness
-                out = out.append(out6.reset_index())
-
-        keep = set(out.columns) - {'scaling', 'matrix', 'exposure', 'exposure_unit', 'er_function', 0}
-        out = out[list(keep)].set_index(list(keep - {VALUE_COLUMN, FORECAST_COLUMN}))
-
-        return self.clean_computing(out)
 
 
 class DiseaseBurden(Ovariable):
