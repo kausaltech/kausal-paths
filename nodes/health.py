09d4336d80102d551a3c2d605f75ef46764a7a38
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2022-02-17 13:24:53 +0200

    Changed HIA to use datasets not pint.context for data

--- nodes/health.py
+++ nodes/health.py
@@ -1,24 +1,9 @@
-# from logging import exception, log
-# from types import FunctionType
-
-# import dvc_pandas
-# from graphene.types.scalars import Float, Int
-# from pint.registry import ContextCacheOverlay
-# from pint_pandas.pint_array import PintArray
-from params.param import NumberParameter, PercentageParameter, StringParameter
-# from typing import Dict, List
 import pandas as pd
-# import pint
-from .context import unit_registry
 import numpy as np
-# import math
-# import copy
 
-# from common.i18n import TranslatedString
+from params.param import NumberParameter, PercentageParameter, StringParameter
+from .context import unit_registry
 from .constants import FORECAST_COLUMN, VALUE_COLUMN, YEAR_COLUMN, FORECAST_x, FORECAST_y, VALUE_x, VALUE_y
-# from .node import Context, Node
-# from .exceptions import NodeError
-
 from .simple import AdditiveNode, FixedMultiplierNode, SimpleNode
 from .ovariable import Ovariable, OvariableFrame
 
@@ -93,44 +78,23 @@ class EmissionByFactor(Ovariable):
 
 
 class ExposureInhalation(Ovariable):
-    allowed_parameters = [
-        StringParameter(local_id='emission_heights'),
-        StringParameter(local_id='population_densities'),
-        StringParameter(local_id='pollutants'),
-    ] + Ovariable.allowed_parameters
 
     quantity = 'exposure'
 
     def compute(self):
-        em_heights = self.get_parameter_value('emission_heights')
-        pop_densities = self.get_parameter_value('population_densities')
-        pollutants = self.get_parameter_value('pollutants')
         inhalation_rate = unit_registry('inhalation_rate')
         emission = OvariableFrame(self.get_input('emissions'))
         population = OvariableFrame(self.get_input('population'))
-        intake_fraction = pd.DataFrame()
+        df = self.get_input_dataset()
 
-        for em_height in em_heights:
-            for pop_density in pop_densities:
-                contxt = 'if_' + em_height + '_' + pop_density
-                for pollutant in pollutants:
-                    value = unit_registry('intake_fraction_' + pollutant).to('ppm', contxt)
-                    intake_fraction = intake_fraction.append(pd.DataFrame({
-                        'Pollutant': [pollutant],
-                        'Emission_height': [em_height],
-                        'Population_density': [pop_density],
-                        FORECAST_COLUMN: [False],
-                        VALUE_COLUMN: [value]
-                    }))
-        indices = ['Pollutant', 'Emission_height', 'Population_density']
-        intake_fraction = OvariableFrame(intake_fraction.set_index(indices))
+        inde = list(set(df.columns) - {VALUE_COLUMN})
+        intake_fraction = OvariableFrame(df.set_index(inde))
 
         exposure = emission * intake_fraction * unit_registry('1 person/year')
         exposure = exposure / (population * inhalation_rate)
         grouping = ['Pollutant', YEAR_COLUMN]
         exposure = exposure.aggregate_by_column(grouping, 'sum')
         exposure[VALUE_COLUMN] = self.ensure_output_unit(exposure[VALUE_COLUMN])
-        self.print_outline(exposure)
 
         return exposure
 
@@ -165,9 +129,9 @@ class PopulationAttributableFraction(Ovariable):
         if YEAR_COLUMN not in exposure.index.names:  # FIXME not the right place for this fix
             assert len(exposure.index.names) == 1
             exposure.index.names = [YEAR_COLUMN]
-        exposure[VALUE_COLUMN] = exposure[VALUE_COLUMN].pint.to('mg/kg/d', 'exposure_generic')
         frexposed = self.get_input('fraction')
         erf_contexts = self.get_parameter_value('erf_contexts')
+        df = self.get_input_dataset()
 
         def postprocess_relative(rr, frexposed):
             r = frexposed * (rr - 1)
@@ -186,38 +150,49 @@ class PopulationAttributableFraction(Ovariable):
 
         for erf_context in erf_contexts:
 
-            route = routes[unit_registry('route').to('dimensionless', erf_context).m]
-            erf_type = unit_registry('er_function').to('dimensionless', erf_context)
-            p_illness = unit_registry('p_illness').to('dimensionless', erf_context)
-            incidence = unit_registry('incidence').to('case/personyear', erf_context)
-            period = unit_registry('period').to('d/incident', erf_context)
+            p_illness = unit_registry('p_illness')
+            incidence = unit_registry('incidence')
+            period = unit_registry('period')
+            erf = df.loc[df.Erf_context == erf_context].reset_index()
+            route = erf.Route[0]
+
+            assert len(erf) == 1
+
+            if 'Er_function' in erf.columns:
+                erf_type = erf.Er_function[0]
+                print(type(erf_type))
+                print(erf_type)
 
             if 'Pollutant' in exposure.index.names:
-                exposure_agent = erf_context.split('_x_')[0]
+                exposure_agent = erf_context.split(' ')[0]
                 exposure_agent = exposure.index.get_level_values('Pollutant') == exposure_agent
                 exposure2 = OvariableFrame(exposure.loc[exposure_agent].copy())
             else:
                 exposure2 = OvariableFrame(exposure.copy())
 
-            if erf_type == 1:  # unit risk
-                slope = unit_registry('erf_param_inv_' + route).to('kg d/mg', erf_context)
-                threshold = unit_registry('erf_param_' + route).to('mg/kg/d', erf_context)
+            if erf_context.split(' ')[0] == 'dioxin':  # FIXME Not a good place for this adjustment
+                exposure2[VALUE_COLUMN] = exposure2[VALUE_COLUMN].pint.to('mg/kg/d', 'exposure_generic')
+                self.print_pint_df(exposure2)
+
+            if erf_type == 'unit risk':
+                slope = erf['param_inv_' + route][0]
+                threshold = erf['param_' + route][0]
                 target_population = unit_registry('1 person')
                 out = (exposure2 - threshold) * slope * frexposed * p_illness
                 out = (out / target_population / period) / incidence
 
-            elif erf_type == 2:  # step function
-                lower = unit_registry('erf_param_' + route).to('mg/kg/d', erf_context)
-                upper = unit_registry('erf_param2_' + route).to('mg/kg/d', erf_context)
+            elif erf_type == 'step function':
+                lower = erf['param_' + route][0]
+                upper = erf['param2_' + route][0]
                 target_population = unit_registry('1 person')
                 tmp = OvariableFrame(exposure2.copy())
                 out = (tmp >= lower) * 1
                 out = out * (exposure2 <= upper) * frexposed * p_illness
                 out = (out / target_population / period) / incidence
 
-            elif erf_type == 3:  # relative risk
-                beta = unit_registry('erf_param_inv_' + route).to('kg d /mg', erf_context)
-                threshold = unit_registry('erf_param_' + route).to('mg/kg/d', erf_context)
+            elif erf_type == 'relative risk':
+                beta = erf['param_inv_' + route][0]
+                threshold = erf['param_' + route][0]
                 out = exposure2 - threshold
                 # out[VALUE_COLUMN] = np.where(  # FIXME
                 #     out[VALUE_COLUMN] < unit_registry('0 mg/kg/d'),
@@ -226,37 +201,36 @@ class PopulationAttributableFraction(Ovariable):
                 out = (out * beta).exp()
                 out = postprocess_relative(rr=out, frexposed=frexposed)
 
-            elif erf_type == 4:  # Relative Hill
-                Imax = unit_registry('erf_param_scale').to('dimensionless', erf_context)
-                ed50 = unit_registry('erf_param_ingestion').to('mg/kg/d', erf_context)
+            elif erf_type == 'relative Hill':
+                Imax = erf['param_scale'][0]
+                ed50 = erf['param_' + route][0]
                 tmp = OvariableFrame(exposure2.copy())
                 out = (tmp * Imax) / (exposure2 + ed50) + 1
                 out = postprocess_relative(rr=out, frexposed=frexposed)
 
-            elif erf_type == 5:  # beta poisson approximation
-                p1 = unit_registry('erf_param_ingestion').to('mg/kg/d', erf_context)
-                p2 = unit_registry('erf_param_scale').to('dimensionless', erf_context)
+            elif erf_type == 'beta poisson approximation':
+                p1 = erf['param_' + route][0]
+                p2 = erf['param_scale'][0]
                 out = ((exposure2 / p2 + 1) ** (p1 * -1) * -1 + 1) * frexposed
                 out = (out / incidence * p_illness)
 
-            elif erf_type == 6:  # exact beta poisson # FIXME Logical error with units!
-                p1 = unit_registry('erf_param_ingestion').to('mg/kg/d', erf_context)
-                p2 = unit_registry('erf_param_ingestion2').to('mg/kg/d', erf_context)
+            elif erf_type == 'exact beta poisson':
+                p1 = erf['param_' + route][0]
+                p2 = erf['param2_' + route][0]
                 out = ((p1 / (p1 + p2) * exposure2 * -1).exp() * -1 + 1) * frexposed
                 out = out / incidence * p_illness
 
-            elif erf_type == 7:  # exponential
-                k = unit_registry('erf_param_inv_ingestion').to('kg d/mg', erf_context)
+            elif erf_type == 'exponential':
+                k = erf['param_inv_' + route][0]
                 out = ((k * exposure2 * -1).exp() * -1 + 1) * frexposed
                 out = out / incidence * p_illness
 
-            elif erf_type == 8:  # polynomial
-                print(erf_context)
-                p0 = unit_registry('erf_param_poly0').to('dimensionless', erf_context)
-                p1 = unit_registry('erf_param_poly1').to('(kg d / mg)', erf_context)
-                p2 = unit_registry('erf_param_poly2').to('(kg d / mg)^2', erf_context)
-                p3 = unit_registry('erf_param_poly3').to('(kg d / mg)^3', erf_context)
-                threshold = unit_registry('erf_param_exposure').to('mg/kg/d', erf_context)
+            elif erf_type == 'polynomial':
+                p0 = erf['param_poly0_' + route][0]
+                p1 = erf['param_poly1_' + route][0]
+                p2 = erf['param_poly2_' + route][0]
+                p3 = erf['param_poly3_' + route][0]
+                threshold = erf['param_' + route][0]
                 out = exposure2 - threshold
                 tmp = OvariableFrame(out.copy())
                 tmp1 = OvariableFrame(out.copy())
