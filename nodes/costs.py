7080431e261e09dabde85a39f50a8c9fa1d317f9
Jouni Tuomisto <jouni.tuomisto@kausal.tech>
2022-09-14 12:33:06 +0300

    Removed redundant CostNode

--- nodes/costs.py
+++ nodes/costs.py
@@ -12,46 +12,6 @@ from .ovariable import Ovariable, OvariableFrame
 from .actions.energy_saving import BuildingEnergySavingAction
 
 
-class CostNode(Ovariable):
-    allowed_parameters = [
-        NumberParameter(local_id='investment_lifetime'),
-        NumberParameter(local_id='investment_cost'),
-        NumberParameter(local_id='operation_cost'),
-        NumberParameter(local_id='investment_years'),
-        NumberParameter(local_id='investment_numbers'),
-    ] + Ovariable.allowed_parameters
-
-    quantity = 'currency'
-
-    def compute(self):
-        costs = self.get_input('currency')
-        investment_cost = self.get_parameter_value('investment_cost') * self.get_parameter('investment_cost').unit
-        operation_cost = self.get_parameter_value('operation_cost') * self.get_parameter('operation_cost').unit
-        investment_lifetime = self.get_parameter_value('investment_lifetime')
-        investment_years = self.get_parameter_value('investment_years')
-        investment_numbers = self.get_parameter_value('investment_numbers') * self.get_parameter(
-            'investment_numbers').unit
-
-        discount_rate = self.context.get_parameter_value('discount_rate')
-        discount = 1
-
-        for time in costs.reset_index()[YEAR_COLUMN]:
-            change = unit_registry('0 kEUR')
-            for j in range(len(investment_years)):
-                assert len(investment_years) == len(investment_numbers)
-                year = investment_years[j]
-                number = investment_numbers[j]
-                if time == year:
-                    change = change + (investment_cost * number)
-                if time >= year and time < year + investment_lifetime:
-                    change = change + (operation_cost * number)
-            if costs.at[time, FORECAST_COLUMN]:
-                discount = discount * (1 + discount_rate)  # FIXME Discounting should NOT be shown on graph
-            costs.at[time, VALUE_COLUMN] = (change + costs.at[time, VALUE_COLUMN]) / discount
-
-        return(costs)
-
-
 class SocialCost(SimpleNode):
 
     def compute(self):
