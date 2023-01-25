1eb4dd19733f3f243d28c7c7da1f73b6d4686de9
Juha Yrjölä <juha.yrjola@iki.fi>
2023-01-25 15:49:45 +0000

    Fix load_dvc_dataset

--- datasets/management/commands/load_dvc_dataset.py
+++ datasets/management/commands/load_dvc_dataset.py
@@ -1,9 +1,11 @@
 import pint_pandas
+import polars as pl
 import pandas as pd
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from datasets.models import Dataset, DatasetDimension, Dimension, DatasetMetric
+from common import polars as ppl
 from nodes.datasets import JSONDataset
 from nodes.models import InstanceConfig
 from nodes.node import Context
@@ -15,34 +17,43 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('instance', metavar='INSTANCE_ID', type=str, nargs=1)
-        parser.add_argument('datasets', metavar='DATASET_ID', type=str, nargs='+')
+        parser.add_argument('datasets', metavar='DATASET_ID', type=str, nargs='*')
+        parser.add_argument('--force', type=bool)
 
-    def sync_dataset(self, ic: InstanceConfig, ctx: Context, ds_id: str):
+    def sync_dataset(self, ic: InstanceConfig, ctx: Context, ds_id: str, force: bool = False):
         ds = ctx.load_dvc_dataset(ds_id)
-        df = ds.df
+        df = ppl.from_pandas(ds.df)
         dims = []
         metrics = []
-        for name, dt in list(df.dtypes.items()):
-            assert isinstance(name, str)
-            if isinstance(dt, pint_pandas.PintType):
-                metrics.append(DatasetMetric(identifier=name, label=name, unit=str(dt.units)))
-            elif isinstance(dt, (str, pd.CategoricalDtype)):
-                dim = ctx.dimensions[name]
-                ic.sync_dimension(dim)
-                df[name] = dim.series_to_ids(df[name])
-                dims.append(ic.dimensions.get(identifier=name))
-            elif name.lower() == FORECAST_COLUMN.lower():
-                if name != FORECAST_COLUMN:
-                    df = df.rename(columns={name: FORECAST_COLUMN})
-            elif name.lower() == YEAR_COLUMN.lower():
-                if name != YEAR_COLUMN:
-                    df = df.rename(columns={name: YEAR_COLUMN})
+        meta = df.get_meta()
+        all_cols = list(df.columns)
+        for col in meta.metric_cols:
+            unit = meta.units[col]
+            metrics.append(DatasetMetric(identifier=col, label=col, unit=str(unit)))
+            all_cols.remove(col)
+        for col in meta.dim_ids:
+            dim = ctx.dimensions[col]
+            ic.sync_dimension(dim)
+            df = df.with_column(dim.series_to_ids_pl(df[col]))
+            dims.append(ic.dimensions.get(identifier=col))
+            all_cols.remove(col)
+
+        for col in all_cols:
+            if col.lower() == FORECAST_COLUMN.lower():
+                if col != FORECAST_COLUMN:
+                    df = df.rename({col: FORECAST_COLUMN})
+            elif col.lower() == YEAR_COLUMN.lower():
+                if col != YEAR_COLUMN:
+                    df = df.rename({col: YEAR_COLUMN})
             else:
-                raise Exception("Unknown column '%s': %s" % (name, dt))
+                raise Exception("Unknown column '%s'" % col)
 
         identifier = ds_id.split('/')[-1]
         obj = ic.datasets.filter(identifier=identifier).first()
-        if obj is None:
+        if obj is None or force:
+            if obj is not None:
+                print("Removing existing dataset")
+                obj.delete()
             obj = Dataset(identifier=identifier, instance=ic, name=ds_id)
             obj.save()
             for m in metrics:
@@ -53,15 +64,23 @@ class Command(BaseCommand):
             obj.save()
         else:
             print("Dataset %s already exists" % identifier)
+            return
 
-        df = df.set_index([YEAR_COLUMN, *[dim.identifier for dim in dims]])
-        obj.table = JSONDataset.serialize_df(df, add_uuids=True)
+        pdf = df.to_pandas()
+        obj.table = JSONDataset.serialize_df(pdf, add_uuids=True)
         obj.save()
 
     def handle(self, *args, **options):
         instance_id = options['instance'][0]
         ic = InstanceConfig.objects.get(identifier=instance_id)
         ctx = ic.get_instance().context
+        if not options['datasets']:
+            print("Available datasets:")
+            ctx.generate_baseline_values()
+            for ds_id, ds in ctx.dvc_datasets.items():
+                print(ds_id)
+            exit()
+
         for ds_id in options['datasets']:
             with transaction.atomic():
-                self.sync_dataset(ic, ctx, ds_id)
+                self.sync_dataset(ic, ctx, ds_id, options['force'])
