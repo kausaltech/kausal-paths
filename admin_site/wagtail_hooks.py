e04d192e525f064fa414ea633d768bb84991483b
Juha Yrjölä <juha.yrjola@iki.fi>
2023-02-07 21:00:21 +0200

    Add a place to customize admin styles

---
+++ admin_site/wagtail_hooks.py
@@ -0,0 +1,9 @@
+from django.utils.html import format_html
+from django.templatetags.static import static
+
+from wagtail import hooks
+
+
+@hooks.register('insert_global_admin_css')
+def global_admin_css():
+    return format_html('<link rel="stylesheet" href="{}">', static('css/admin-styles.css'))
