3bdbfc7c9ac303bce1ddf83f4d85c2d071dc30da
Juha Yrjölä <juha.yrjola@iki.fi>
2024-06-18 10:58:28 +0300

    Fix PathsAdminModelForm for i18n support

--- admin_site/forms.py
+++ admin_site/forms.py
@@ -10,21 +10,24 @@ from nodes.models import InstanceConfig
 class PathsAdminModelForm(WagtailAdminModelForm):
     admin_instance: InstanceConfig | None = None
 
-    def prune_i18n(self):
-        pass
-
-    def __init__(self, *args, **kwargs):
-        self.admin_instance = kwargs.pop("admin_instance", None)
-        super().__init__(*args, **kwargs)
+    def prune_i18n_fields(self):
         model: type[Model] = self._meta.model
         i18n_field = get_i18n_field(model)
         if not i18n_field:
             return
-
         other_langs = self.admin_instance.other_languages if self.admin_instance is not None else []
-        for field in i18n_field.get_translated_fields():
-            langs = get_available_languages(include_default=False)
+        for base_field_name in i18n_field.fields:
+            langs = list(get_available_languages(include_default=True))
             for lang in langs:
-                fn = build_localized_fieldname(field.name, lang)
+                fn = build_localized_fieldname(base_field_name, lang)
                 if fn in self.fields and lang not in other_langs:
                     del self.fields[fn]
+
+    def save(self, commit=True):
+        obj = super().save(commit)
+        return obj
+
+    def __init__(self, *args, **kwargs):
+        self.admin_instance = kwargs.pop("admin_instance", None)
+        super().__init__(*args, **kwargs)
+        self.prune_i18n_fields()
