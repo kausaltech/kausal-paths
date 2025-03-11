5414132e0d998d769af4905207ddaa5714f6224f
Timo Tuominen <tituomin@gmail.com>
2025-03-11 12:43:17 +0200

    style: organize imports

--- admin_site/dataset_admin.py
+++ admin_site/dataset_admin.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from wagtail.snippets.models import register_snippet
-from wagtail.snippets.views.snippets import SnippetViewSet, CreateView
+from wagtail.snippets.views.snippets import CreateView, SnippetViewSet
 
-from kausal_paths_extensions.dataset_editor import DatasetViewSet
 from kausal_common.datasets.config import dataset_config
 from kausal_common.datasets.models import DatasetSchema
+
+from kausal_paths_extensions.dataset_editor import DatasetViewSet
 
 
 class DatasetSchemaCreateView(CreateView):
