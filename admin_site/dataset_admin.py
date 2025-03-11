c37529b33b983668586cc723d010a1a2319c4e07
Timo Tuominen <tituomin@gmail.com>
2025-03-11 12:58:25 +0200

    fix: type error

--- admin_site/dataset_admin.py
+++ admin_site/dataset_admin.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from wagtail.admin.forms.models import WagtailAdminModelForm
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import CreateView, SnippetViewSet
 
@@ -9,15 +10,18 @@ from kausal_common.datasets.config import dataset_config
 from kausal_common.datasets.models import DatasetSchema
 
 from kausal_paths_extensions.dataset_editor import DatasetViewSet
+from users.models import User
 
 
-class DatasetSchemaCreateView(CreateView):
+class DatasetSchemaCreateView(CreateView[DatasetSchema, WagtailAdminModelForm[DatasetSchema, User]]):
     def get_success_url(self):
         if not dataset_config.SCHEMA_HAS_SINGLE_DATASET:
             return super().get_success_url()
         if not self.form.instance.datasets.exists():
             return super().get_success_url()
         only_dataset = self.form.instance.datasets.first()
+        if only_dataset is None:
+            return super().get_success_url()
         return reverse(DatasetViewSet().get_url_name('edit'), args=(only_dataset.pk,))
 
 
