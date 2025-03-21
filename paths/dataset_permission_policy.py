3a47d9248b23e580c33a12e458d871c7f7aecec2
Bernhard Bliem <bernhard@bliem.xyz>
2025-03-21 09:29:56 +0100

    Use permission policies in datasets REST API

--- paths/dataset_permission_policy.py
+++ paths/dataset_permission_policy.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeGuard
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, TypeGuard, TypeVar, override
 
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 
 from kausal_common.datasets.models import (
     DataPoint,
     Dataset,
     DatasetQuerySet,
     DatasetSchema,
+    DataSource,
 )
 from kausal_common.models.permission_policy import (
     BaseObjectAction,
@@ -24,39 +26,40 @@ from paths.context import realm_context
 from nodes.roles import instance_admin_role, instance_viewer_role
 
 if TYPE_CHECKING:
+    from kausal_common.models.permissions import PermissionedModel
+
     from nodes.models import InstanceConfig
     from users.models import User
 
+_M = TypeVar('_M', bound='PermissionedModel')
+_QS = TypeVar('_QS', bound=QuerySet, default=QuerySet[_M])
 
-class DatasetSchemaPermissionPolicy(ModelPermissionPolicy[DatasetSchema, 'InstanceConfig', PermissionedQuerySet[DatasetSchema]]):
-    """Permission policy for DatasetSchema, based on its scope (InstanceConfig)."""
 
-    def __init__(self):
-        from kausal_common.datasets.models import DatasetSchema  # TODO why import here?
+class InstanceConfigScopedPermissionPolicy(ModelPermissionPolicy[_M, _QS]):
+    """Permission policy for models that have one or many InstanceConfig objects as scope."""
 
+    def __init__(self, model: type[_M]):
         from nodes.models import InstanceConfig
 
-        self.model = DatasetSchema
+        self.model = model
         self.instance_admin_role = instance_admin_role
         self.instance_viewer_role = instance_viewer_role
         self.ic_model = InstanceConfig
-        super().__init__(DatasetSchema)
+        super().__init__(model)
 
+    @override
     def is_create_context_valid(self, context: Any) -> TypeGuard[InstanceConfig]:
         from nodes.models import InstanceConfig
         return isinstance(context, InstanceConfig)
 
-    def get_instance_configs_for_schema(self, schema: DatasetSchema) -> list[int]:
-        """Get IDs of all InstanceConfigs this schema is scoped for."""
-        from nodes.models import InstanceConfig
-        ic_content_type = ContentType.objects.get_for_model(InstanceConfig)
-        return list(schema.scopes.filter(
-            scope_content_type=ic_content_type
-        ).values_list('scope_id', flat=True))
+    @abstractmethod
+    def get_instance_configs_for_obj(self, obj: _M) -> list[int]:
+        """Get IDs of all InstanceConfigs this obj is scoped for."""
 
-    def user_has_perm(self, user: User, action: BaseObjectAction, obj: DatasetSchema) -> bool:
+    @override
+    def user_has_perm(self, user: User, action: BaseObjectAction, obj: _M) -> bool:
         active_instance = realm_context.get().realm
-        instance_ids = self.get_instance_configs_for_schema(obj)
+        instance_ids = self.get_instance_configs_for_obj(obj)
         if active_instance is None or active_instance.pk not in instance_ids:
             return False
         # TODO HERE
@@ -68,12 +71,36 @@ class DatasetSchemaPermissionPolicy(ModelPermissionPolicy[DatasetSchema, 'Instan
             return user.has_instance_role(self.instance_viewer_role, active_instance)
         return False
 
-    def anon_has_perm(self, action: BaseObjectAction, obj: DatasetSchema) -> bool:
+    @override
+    def anon_has_perm(self, action: BaseObjectAction, obj: _M) -> bool:
         return False
 
+    @override
     def user_can_create(self, user: User, context: InstanceConfig) -> bool:
         return user.has_instance_role(self.instance_admin_role, context)
 
+    @override
+    def construct_perm_q_anon(self, action: BaseObjectAction) -> Q | None:
+        return None
+
+
+class DatasetSchemaPermissionPolicy(InstanceConfigScopedPermissionPolicy[DatasetSchema]):
+    """Permission policy for DatasetSchema, based on its scope (InstanceConfig)."""
+
+    def __init__(self):
+        from kausal_common.datasets.models import DatasetSchema  # TODO why import here?
+        super().__init__(DatasetSchema)
+
+    @override
+    def get_instance_configs_for_obj(self, obj: DatasetSchema) -> list[int]:
+        """Get IDs of all InstanceConfigs this schema is scoped for."""
+        from nodes.models import InstanceConfig
+        ic_content_type = ContentType.objects.get_for_model(InstanceConfig)
+        return list(obj.scopes.filter(
+            scope_content_type=ic_content_type
+        ).values_list('scope_id', flat=True))
+
+    @override
     def construct_perm_q(self, user: User, action: BaseObjectAction) -> Q | None:
         from nodes.models import InstanceConfig
         ic_content_type = ContentType.objects.get_for_model(InstanceConfig)
@@ -92,9 +119,6 @@ class DatasetSchemaPermissionPolicy(ModelPermissionPolicy[DatasetSchema, 'Instan
 
         return admin_q
 
-    def construct_perm_q_anon(self, action: BaseObjectAction) -> Q | None:
-        return None
-
 
 class DatasetPermissionPolicy(ParentInheritedPolicy[Dataset, DatasetSchema, DatasetQuerySet]):
     """Permission policy for Dataset, inheriting from its schema."""
@@ -103,13 +127,16 @@ class DatasetPermissionPolicy(ParentInheritedPolicy[Dataset, DatasetSchema, Data
         from kausal_common.datasets.models import Dataset, DatasetSchema
         super().__init__(Dataset, DatasetSchema, 'schema')
 
+    @override
     def user_has_perm(self, user: User, action: ObjectSpecificAction, obj: Dataset) -> bool:
         parent_obj = self.get_parent_obj(obj)
         return self.parent_policy.user_has_perm(user, action, parent_obj)
 
+    @override
     def anon_has_perm(self, action: BaseObjectAction, obj: Dataset) -> bool:
         return False
 
+    @override
     def user_can_create(self, user: User, context: DatasetSchema) -> bool:
         return self.parent_policy.user_has_perm(user, 'change', context)
 
@@ -121,12 +148,49 @@ class DataPointPermissionPolicy(ParentInheritedPolicy[DataPoint, Dataset, Permis
         from kausal_common.datasets.models import DataPoint, Dataset
         super().__init__(DataPoint, Dataset, 'dataset')
 
+    @override
     def user_has_perm(self, user: User, action: ObjectSpecificAction, obj: DataPoint) -> bool:
         parent_obj = self.get_parent_obj(obj)
         return self.parent_policy.user_has_perm(user, action, parent_obj)
 
+    @override
     def anon_has_perm(self, action: BaseObjectAction, obj: DataPoint) -> bool:
         return False
 
+    @override
     def user_can_create(self, user: User, context: Dataset) -> bool:
         return self.parent_policy.user_has_perm(user, 'change', context)
+
+
+class DataSourcePermissionPolicy(InstanceConfigScopedPermissionPolicy[DataSource]):
+    """Permission policy for DataSource, based on its scope (InstanceConfig)."""
+
+    def __init__(self):
+        super().__init__(DataSource)
+
+    @override
+    def get_instance_configs_for_obj(self, obj: DataSource) -> list[int]:
+        from nodes.models import InstanceConfig
+        ic_content_type = ContentType.objects.get_for_model(InstanceConfig)
+        if obj.scope_content_type != ic_content_type:
+            return []
+        return [obj.scope_id]
+
+    @override
+    def construct_perm_q(self, user: User, action: BaseObjectAction) -> Q | None:
+        from nodes.models import InstanceConfig
+        ic_content_type = ContentType.objects.get_for_model(InstanceConfig)
+
+        admin_q = Q(
+            scope_content_type=ic_content_type,
+            scope_id__in=self.instance_admin_role.get_instances_for_user(user)
+        )
+
+        if action == 'view':
+            viewer_q = Q(
+                scope_content_type=ic_content_type,
+                scope_id__in=self.instance_viewer_role.get_instances_for_user(user)
+            )
+            return admin_q | viewer_q
+
+        return admin_q
