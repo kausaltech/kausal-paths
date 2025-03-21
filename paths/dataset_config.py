a7184703610f92896f03afc3b91862562ed7b06e
Bernhard Bliem <bernhard@bliem.xyz>
2025-03-21 09:27:17 +0100

    Fix missing underscore in DATA_SOURCE_DEFAULT_SCOPE_CONTENT_TYPE

--- paths/dataset_config.py
+++ paths/dataset_config.py
@@ -20,7 +20,7 @@ def schema_default_scope():
     from paths.context import realm_context
     return realm_context.get().realm
 
-DATASOURCE_DEFAULT_SCOPE_CONTENT_TYPE: tuple[str, str] =  ('nodes', 'instanceconfig')
+DATA_SOURCE_DEFAULT_SCOPE_CONTENT_TYPE: tuple[str, str] =  ('nodes', 'instanceconfig')
 SCHEMA_HAS_SINGLE_DATASET: bool = True
 SCHEMA_DEFAULT_SCOPE_FUNCTION: Callable[[], Model] | None = schema_default_scope
 SHOW_DATASETS_IN_MENU: bool = True
