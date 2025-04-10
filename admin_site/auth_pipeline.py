f31950e240c57be7e7c73a8e6c5687e3f875d7a6
Matias Wargelin <matias.wargelin@kausal.tech>
2025-04-10 14:30:12 +0300

    Improve readability
    Improve readability using full names for the variables.

--- admin_site/auth_pipeline.py
+++ admin_site/auth_pipeline.py
@@ -22,35 +22,39 @@ def assign_roles(
     from frameworks.roles import framework_admin_role, framework_viewer_role
     from nodes.roles import instance_admin_role, instance_viewer_role
 
-    fw_roles: list[FrameworkRoleDef] = details.get('framework_roles', [])
-    if user is None or not fw_roles:
+    framework_roles: list[FrameworkRoleDef] = details.get('framework_roles', [])
+    if user is None or not framework_roles:
         return
 
-    for role in fw_roles:
+    for role in framework_roles:
         if not role.role_id:
             continue
 
-        fw_obj = Framework.objects.filter(identifier=role.framework_id).first()
-        if not fw_obj:
+        framework_object = Framework.objects.filter(identifier=role.framework_id).first()
+        if not framework_object:
             logger.error("Framework '%s' not found" % role.framework_id)
             continue
+
         user.extra.set_framework_role(role)
         user.save(update_fields=['extra'])
+
         if role.role_id == FRAMEWORK_ADMIN_ROLE:
-            framework_admin_role.assign_user(fw_obj, user)
+            framework_admin_role.assign_user(framework_object, user)
         elif role.role_id == FRAMEWORK_VIEWER_ROLE:
-            framework_viewer_role.assign_user(fw_obj, user)
+            framework_viewer_role.assign_user(framework_object, user)
         elif role.role_id in (INSTANCE_ADMIN_ROLE, INSTANCE_VIEWER_ROLE):
-            fwcs = FrameworkConfig.objects.filter(
-                framework=fw_obj, organization_identifier=role.org_id
+            framework_configs = FrameworkConfig.objects.filter(
+                framework=framework_object, organization_identifier=role.org_id
             ).exclude(organization_identifier__isnull=True)
-            for fwc in fwcs:
-                ic = fwc.instance_config
+
+            for framework_config in framework_configs:
+                instance_config = framework_config.instance_config
                 if role.role_id == INSTANCE_ADMIN_ROLE:
-                    instance_admin_role.assign_user(ic, user)
+                    instance_admin_role.assign_user(instance_config, user)
                 elif role.role_id == INSTANCE_VIEWER_ROLE:
-                    instance_viewer_role.assign_user(ic, user)
-            if not fwcs:
+                    instance_viewer_role.assign_user(instance_config, user)
+
+            if not framework_configs:
                 logger.info("Framework config for org UID '%s' does not yet exist" % (role.org_id))
         else:
             logger.error("Unknown role '%s'" % role.role_id)
