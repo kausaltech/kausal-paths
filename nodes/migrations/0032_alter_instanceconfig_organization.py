dbf769ddcf51a6f03a748f3da94e2661919e8a8b
Bernhard Bliem <bernhard@bliem.xyz>
2025-06-25 15:48:32 +0200

    Add organization FK to InstanceConfig
    Since every InstanceConfig needs an organization, the migration creates a new Organization for each InstanceConfig. This is done using a hack that requires no organizations exist yet in the database.

---
+++ nodes/migrations/0032_alter_instanceconfig_organization.py
@@ -0,0 +1,17 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nodes', '0031_create_instanceconfig_organizations'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='instanceconfig',
+            name='organization',
+            field=models.ForeignKey(help_text='The main organization for the instance', on_delete=django.db.models.deletion.PROTECT, related_name='instances', to='orgs.organization', verbose_name='organization'),
+        ),
+    ]
