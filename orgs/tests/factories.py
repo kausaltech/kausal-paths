e1118becddb6ae58a0cbc5366e9f83d77b0a0a38
Bernhard Bliem <bernhard@bliem.xyz>
2025-06-26 22:46:47 +0200

    Implement OrganizationFactory

---
+++ orgs/tests/factories.py
@@ -0,0 +1,24 @@
+from wagtail.rich_text import RichText
+
+from factory import Sequence
+from factory.django import DjangoModelFactory
+
+from orgs.models import Organization
+
+
+class OrganizationFactory(DjangoModelFactory):
+    class Meta:
+        model = Organization
+
+    name = Sequence(lambda i: f"Organization {i}")
+    abbreviation = Sequence(lambda i: f'org{i}')
+    description = RichText("<p>Description</p>")
+    url = 'https://example.org'
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs) -> Organization:  # noqa: ARG003
+        parent = kwargs.pop('parent', None)
+        node = Organization(*args, **kwargs)
+        if parent:
+            return parent.add_child(instance=node)
+        return Organization.add_root(instance=node)
