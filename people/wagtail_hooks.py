247dbe9a5a7fee1e764395b594155a6e3dc06c73
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-07-17 16:06:11 +0300

    People and organizations not to be shown in admin top menu

--- people/wagtail_hooks.py
+++ people/wagtail_hooks.py
@@ -1,12 +1,13 @@
+from __future__ import annotations
+
+from django.utils.translation import gettext_lazy as _
+from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-from wagtail.admin.filters import DateRangePickerWidget
-from django.utils.translation import gettext_lazy as _
-
-from .models import Person
 
 from dal_select2.widgets import ModelSelect2
+
+from .models import Person
 
 
 class PersonSnippetViewSet(SnippetViewSet):
@@ -14,7 +15,7 @@ class PersonSnippetViewSet(SnippetViewSet):
     menu_label = _('People')
     menu_icon = 'user'
     menu_order = 200
-    add_to_admin_menu = True
+    add_to_admin_menu = False
 
     panels = [
         FieldPanel('first_name'),
@@ -28,7 +29,8 @@ class PersonSnippetViewSet(SnippetViewSet):
     ]
 
     list_display = ['first_name', 'last_name', 'email', 'organization', 'title']
-    list_filter = ['organization', 'created_by']
+
+    # list_filter = ['organization', 'created_by']
     search_fields = ['first_name', 'last_name', 'email', 'title']
     list_per_page = 50
 
