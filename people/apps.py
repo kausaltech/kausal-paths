5116415ac560d61ebfddb284a6f4fbb9dd51e400
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-07-18 19:03:29 +0300

    Person avatar

--- people/apps.py
+++ people/apps.py
@@ -4,3 +4,13 @@ from django.utils.translation import gettext_lazy as _
 class PeopleConfig(AppConfig):
     name = 'people'
     verbose_name = _('People')
+
+    def ready(self):
+        # Register feature detection library
+        from willow.registry import registry
+        try:
+            import rustface.willow
+        except ImportError:
+            pass
+        else:
+            registry.register_plugin(rustface.willow)
