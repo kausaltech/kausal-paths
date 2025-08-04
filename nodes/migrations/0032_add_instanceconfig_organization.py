867f6a9517202c5571c9a3b332f2f39ca5b0bef6
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-08-04 12:32:33 +0300

    creating instanceconfig orgs in separate migration file

---
+++ nodes/migrations/0032_add_instanceconfig_organization.py
@@ -0,0 +1,18 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('nodes', '0031_alter_nodeconfig_color'),
+        ('orgs', '0002_organizationmetadataadmin_and_more'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='instanceconfig',
+            name='organization',
+            field=models.ForeignKey(null=True, blank=True, on_delete=django.db.models.deletion.PROTECT, to='orgs.organization'),
+        ),
+    ]
