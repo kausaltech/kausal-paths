e72422e5cb07c1802259b6815dd9403bc8260bb9
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-09-01 10:16:20 +0300

    Remove usage of organization internal_abbreviation

--- orgs/wagtail_hooks.py
+++ orgs/wagtail_hooks.py
@@ -112,7 +112,6 @@ class OrganizationViewSet(PathsViewSet):
         ),
         # FieldPanel('logo'),
         TranslatedFieldPanel('abbreviation'),
-        FieldPanel('internal_abbreviation'),
         # Don't allow editing identifiers at this point
         # CondensedInlinePanel('identifiers', panels=[
         #     FieldPanel('namespace'),
