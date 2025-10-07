f53488f9e0584be8595ebf77479805625f2e3adb
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2025-10-07 15:19:01 +0300

    ignore dir e2e-tests in watchfiles_reloader so that auth.setup.ts also works with playwright ui

--- paths/watchfiles_reloader.py
+++ paths/watchfiles_reloader.py
@@ -1,18 +1,28 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Generator, Optional, Sequence, Union
+from typing import Generator, Sequence
+
+from django.utils import autoreload
 
 import watchfiles
-from django.utils import autoreload
 
 
 class DjangoPythonFilter(watchfiles.PythonFilter):
-    def __init__(self, *, ignore_paths: Optional[Sequence[Union[str, Path]]] = None, extra_extensions: Sequence[str] = ()) -> None:
+    def __init__(
+            self,
+            *,
+            ignore_paths: Sequence[str | Path] | None = None,
+            extra_extensions: Sequence[str] = (),
+    ) -> None:
         if 'site-packages' in self.ignore_dirs:
             # We want to watch site-packages, too
             d = list(self.ignore_dirs)
             d.remove('site-packages')
+            self.ignore_dirs = d
+        if 'e2e-tests' not in self.ignore_dirs:
+            d = list(self.ignore_dirs)
+            d.append('e2e-tests')
             self.ignore_dirs = d
         super().__init__(ignore_paths=ignore_paths, extra_extensions=extra_extensions)
 
@@ -21,10 +31,10 @@ class WatchfilesReloader(autoreload.BaseReloader):
     def watched_roots(self, watched_files: list[Path]) -> frozenset[Path]:
         extra_directories = self.directory_globs.keys()
         watched_file_dirs = {f.parent for f in watched_files}
-        sys_paths = set([path for path in autoreload.sys_path_directories() if 'site-packages' in str(path)])
+        sys_paths = {path for path in autoreload.sys_path_directories() if 'site-packages' in str(path)}
         return frozenset((*extra_directories, *watched_file_dirs, *sys_paths))
 
-    def tick(self) -> Generator[None, None, None]:
+    def tick(self) -> Generator[None]:
         watched_files = list(self.watched_files(include_globs=False))
         watched_roots = self.watched_roots(watched_files)
         roots = autoreload.common_roots(watched_roots)
