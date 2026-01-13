6287b10a97ebde72983e2072f07826df2b35b060
Jan-Erik S. <6697396+Bhavatu@users.noreply.github.com>
2026-01-13 13:03:08 +0200

    add post_save signal receivers for NodeDataset

--- nodes/wagtail_hooks.py
+++ nodes/wagtail_hooks.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from django.db import transaction
+from django.db.models.signals import m2m_changed, post_delete, post_save
+from django.dispatch import receiver
 from django.urls import reverse
 from wagtail.admin.admin_url_finder import AdminURLFinder, register_admin_url_finder
 from wagtail.models import ReferenceIndex
@@ -8,10 +11,28 @@ from . import (
     choosers,  # noqa: F401
     node_admin,
 )
-from .models import NodeDataset
+from .models import NodeConfig, NodeDataset
 
-# Register NodeDataset so Wagtail tracks references from it to Dataset
 ReferenceIndex.register_model(NodeDataset)
+
+
+@receiver(post_save, sender=NodeDataset)
+def update_reference_index_on_save(sender, instance, **kwargs):
+    with transaction.atomic():
+        ReferenceIndex.create_or_update_for_object(instance)
+
+
+@receiver(post_delete, sender=NodeDataset)
+def update_reference_index_on_delete(sender, instance, **kwargs):
+    ReferenceIndex.remove_for_object(instance)
+
+
+@receiver(m2m_changed, sender=NodeConfig.datasets.through)
+def update_reference_index_on_m2m_change(sender, instance, action, pk_set, **kwargs):
+    if action == 'post_add':
+        with transaction.atomic():
+            for node_dataset in NodeDataset.objects.filter(node=instance, dataset_id__in=pk_set):
+                ReferenceIndex.create_or_update_for_object(node_dataset)
 
 
 class NodeDatasetAdminURLFinder(AdminURLFinder):
