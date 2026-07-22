565ed3a752bb93ed15da990378fbc9db351b7a90
Juha Yrjölä <juha.yrjola@kausal.tech>
2026-07-22 18:29:47 +0300

    Update polars

--- common/polars.py
+++ common/polars.py
@@ -20,7 +20,7 @@ if typing.TYPE_CHECKING:
     import pandas as pd
     from dvc_pandas.dataset import Dataset as DVCDataset
     from polars._plr import PyDataFrame, PyExpr
-    from polars._typing import JoinStrategy, JoinValidation, MaintainOrderJoin
+    from polars._typing import JoinBuildSide, JoinStrategy, JoinValidation, MaintainOrderJoin
     from polars.type_aliases import ColumnNameOrSelector, IntoExpr, IntoExprColumn
     from rich.repr import RichReprResult
 
@@ -293,6 +293,7 @@ class PathsDataFrame(pl.DataFrame):
         nulls_equal: bool = False,
         coalesce: bool | None = None,
         maintain_order: MaintainOrderJoin | None = None,
+        build_side: JoinBuildSide = 'auto',
     ) -> pl.DataFrame:
         plain_df = pl.DataFrame._from_pydf(self._df)
         df = plain_df.join(
